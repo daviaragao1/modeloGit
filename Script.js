70782ec3f5cf4d66ca460c62a220922d63268468
DAVI <daviaragaoyt@gmail.com>
2023-06-27 13:27:38 -0300

    Testes no javascript

--- Script.js
+++ Script.js
@@ -1,1 +1,15 @@
-console.log("BEM-VINDO.")
+let x;y;z
+x=10
+y=20
+z=30
+console.log("BEM-VINDO")
+
+if(x<y){console.log("10 é menor que 20")
+}
+else{console.log("Default")}
+
+if(y>z){console.log("Default")}
+
+else{
+    console.log("20 é menor que 30")
+}
